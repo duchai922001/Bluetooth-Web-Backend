faf9c9f7e173c60fb9f1832742cdb23edef60a0c
Duchai232 <Duchai757@gmail.com>
2025-01-20 17:36:44 +0700

    deploy serrver

--- src/index.ts
+++ src/index.ts
@@ -23,7 +23,7 @@ app.use(cors(corsOptions));
 connectDB();
 
 mainRoutes(app);
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
