1e1994bc0c214b0d966222b1ccf875bf792da150
duchai922001 <Duchai757@gmail.com>
2025-05-02 18:37:48 +0700

    fix cors

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ app.use(express.json());
 const server = http.createServer(app);
 const io = configureSocketIO(server);
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
